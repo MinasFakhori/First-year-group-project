4a24092af8ebe7c75058c965422f02f5492b5a21
MinasFakhori <minasfakhori1@outlook.com>
2022-03-02 20:42:03 +0000

    Website

---
+++ Website/main.js
@@ -0,0 +1,20 @@
+let sections = document.querySelectorAll("section");
+let nav = document.querySelectorAll("li");
+
+window.addEventListener("scroll", ()=>{
+    let page;
+
+    sections.forEach( section => {
+        let top = section.offsetTop -100;
+        if(pageYOffset >= top){
+            current = section.getAttribute("id")
+        }  
+    })
+
+    nav.forEach(li =>{
+        li.classList.remove("page");
+        if(li.classList.contains(current)){
+            li.classList.add("page")
+        }
+    })
+})
