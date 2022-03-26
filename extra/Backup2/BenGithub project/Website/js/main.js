7fd6f4f0b036481bdebc6f9e6e921dd1a4b8a5a1
MinasFakhori <minasfakhori1@outlook.com>
2022-03-26 12:55:33 +0000

    Added ben and ahmeds work

---
+++ extra/Backup2/BenGithub project/Website/js/main.js
@@ -0,0 +1,74 @@
+let sections = document.querySelectorAll("section");
+let nav = document.querySelectorAll("li");
+let lmSection1 = document.getElementById('learning_materials_sec1');
+let lmSection2 = document.getElementById('learning_materials_sec2');
+let lmSection3 = document.getElementById('learning_materials_sec3');
+let screenWidth = window.innerWidth;
+
+
+
+window.addEventListener("scroll", ()=>{
+    let page;
+
+    sections.forEach( section => {
+        let top = section.offsetTop -150;
+        if(scrollY > top){
+            current = section.getAttribute("id");
+        }  
+    })
+
+    nav.forEach(li =>{
+        li.classList.remove("page");
+        if(li.classList.contains(current)){
+            li.classList.add("page");
+        }
+    })
+
+   
+})
+
+
+ 
+lmSection1.addEventListener("click", () => {
+    document.getElementById("iframe").src = "https://mf600.brighton.domains/ci435_assignment/";
+    
+});
+
+lmSection2.addEventListener("click", () => {
+    document.getElementById("iframe").src = "https://mf600.brighton.domains/ci435_assignment/tutorial.html";
+});
+
+lmSection3.addEventListener("click", () => {
+    document.getElementById("iframe").src = "https://minas.tk/git-and-github";
+});
+
+
+lmSection1.addEventListener('dblclick', () => {
+    window.open("https://google.com");
+    });
+
+lmSection2.addEventListener('dblclick', () => {
+    window.open("https://google.com");
+    });
+    
+lmSection3.addEventListener('dblclick', () => {
+    window.open("https://google.com");
+});    
+
+
+
+if (screenWidth < 1000){
+    lmSection1.addEventListener("click", () => {
+        window.open("https://google.com");
+    });
+    
+    lmSection2.addEventListener("click", () => {
+        window.open("https://google.com");
+    });
+    
+    lmSection3.addEventListener("click", () => {
+        window.open("https://google.com");
+    });
+}
+
+
---
+++ extra/Backup2/BenGithub project/extra/Website/main.js
@@ -0,0 +1,6 @@
+let section1 = document.getElementsByClassName("section1");
+let section2 = document.getElementsByClassName("section2");
+
+
+location.href
+
