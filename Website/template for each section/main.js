b0bada6f9b311ac8145c1fb0e153c6d0edf5ce94
MinasFakhori <minasfakhori1@outlook.com>
2022-04-12 16:43:45 +0100

    done the disabled button

--- Website/template for each section/main.js
+++ Website/template for each section/main.js
@@ -1,17 +1,86 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+let dropdown= document.querySelector(".dropdown");
+let body= document.querySelector("body");
+let fontBtn= document.querySelector(".font");
+let colourBtn= document.querySelector(".colour");
+let zoomBtn= document.querySelector(".zoom");
+let isClicked = false;
+let font = false;
+let colour = false;
+let zoom = false;
+
+
+function fundropdown() {
+    if (isClicked == false){ 
+    dropdown.style.display = "block";
+    isClicked = true;
+    }else if (isClicked == true){
+    dropdown.style.display = "none";
+    isClicked = false;
     }
-  }
+}
+
+
+function changeFont(){
+    if (font == false){ 
+        body.style.fontFamily = "Arial", "Comic Sans";
+        font = true;
+        fontBtn.innerHTML = "Normal Font";
+        dropdown.style.display = "none";
+        isClicked = false;
+    }else if(font == true){
+        fontBtn.innerHTML = "Dyslexia Font"
+        body.style.fontFamily = "Fredoka", "sans-serif";
+        font = false;
+        dropdown.style.display = "none";
+        isClicked = false;
+    }
+}
+
+
+function changeColour(){
+    if(colour == false){
+        body.style.backgroundColor = "#A8F29A";
+        colour = true;
+        colourBtn.innerHTML = "Normal Colour";
+        dropdown.style.display = "none";
+        isClicked = false;
+    }else{
+        body.style.backgroundColor = "#FFFFFF";
+        colour = false;
+        dropdown.style.display = "none";
+        isClicked = false;
+    }
+}
+
+function changeColour(){
+    if(colour == false){
+        body.style.backgroundColor = "#A8F29A";
+        colour = true;
+        colourBtn.innerHTML = "Normal Colour"
+        dropdown.style.display = "none";
+        isClicked = false;
+    }else{
+        body.style.backgroundColor = "#FFFFFF";
+        colour = false;
+        dropdown.style.display = "none";
+        isClicked = false;
+    }
+}
+
+
+function changeZoom(){
+    if( zoom == false){
+        body.style.fontSize = "1.5em";
+        zoomBtn.innerHTML = "Normal Font";
+        zoom = true;
+        dropdown.style.display = "none";
+        isClicked = false;
+    }else{
+        body.style.fontSize = "1em";
+        zoomBtn.innerHTML = "Font";
+        zoom = false;
+        dropdown.style.display = "none";
+        isClicked = false;
+    }
+}
+  
